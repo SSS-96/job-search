f03dade55105ab0701dd48161a9e61819135533b
SSS <sssri.1996@gmail.com>
2024-05-05 17:23:25 +0530

    infinite scrolling implemented

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -5,14 +5,17 @@ import CardContainer from './Card/CardContainer';
 function PageContainer() {
     // setting fetched data for passing to child components
     const [cardData,setCardData] = useState({});
-
+    const [loading, setLoading] = useState(false);
+    const [offset, setOffset] = useState(0);
+    const [limit, setLimit] = useState(5);
     // service call for api
     const fetchData = () => {
+        setLoading(true)
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         const body = JSON.stringify({
-            "limit": 10,
-            "offset": 1
+            "limit": limit,
+            "offset": offset
            });
            
            const requestOptions = {
@@ -21,20 +24,42 @@ function PageContainer() {
             body
            };
            
-           fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
+        fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
             .then((response) => response.json())
             .then((result) => setCardData(result))
             .catch((error) => console.error(error));
+
+        setLoading(false)
     }
 
+    const handleScroll = () => {
+        // Check if user has scrolled to the bottom of the page
+        if (
+            window.innerHeight + document.documentElement.scrollTop ===
+            document.documentElement.offsetHeight
+        ) {
+            setOffset(prevState => prevState + 10);
+            setLimit(prevState => prevState + 10);
+        }
+    };
+
     useEffect(()=>{
-        fetchData()
+        fetchData();
+        window.addEventListener('scroll', handleScroll);
+        // Remove scroll event listener when component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     },[])
+
+    useEffect(()=>{
+        fetchData();
+    },[offset])
 
     return (
         <div className='pageContainer'>
             <FilterContainer/>
-            <CardContainer cardData={cardData}/>
+            {loading ? <p>Loading...</p> : <CardContainer cardData={cardData}/>}
         </div>
     )
 }
