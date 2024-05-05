85f2499abb9bb34f1a6681a10b476524ce7665ee
SSS <sssri.1996@gmail.com>
2024-05-06 03:14:53 +0530

    logic for view more implemented

--- src/components/Card/CardComponent.jsx
+++ src/components/Card/CardComponent.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './card.css';
 
-const obj =         {
+const obj = {
   "jdUid": "cfff35ba-053c-11ef-83d3-06301d0a7178-92012",
   "jdLink": "https://weekday.works",
   "jobDetailsFromCompany": "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
@@ -33,12 +33,38 @@ function CardComponent(props) {
     )
   }
   const renderBody = () => {
+    const TruncatedText = ( text, maxLength ) => {
+      const [expanded, setExpanded] = useState(false);
+
+      const toggleExpand = () => {
+        setExpanded(!expanded);
+      };
+
+      return (
+        <div>
+          {expanded ? (
+            <div>{text}</div>
+          ) : (
+            <div>
+              {text.length > maxLength ? (
+                <React.Fragment>
+                  {text.slice(0, maxLength)}...
+                  <button onClick={toggleExpand}>View more</button>
+                </React.Fragment>
+              ) : (
+                <div>{text}</div>
+              )}
+            </div>
+          )}
+        </div>
+      );
+    };
     return (
       <div className='card-body'>
         {(props?.minJdSalary || props?.maxJdSalary) ? <div className='card-body-salaryInfo'>{`Estimated Salary: ₹ ${props?.minJdSalary} - ${props?.maxJdSalary} LPA  ✅`}</div> : null}
         <div className='card-body-title'>About Company:</div>
         <div className='card-body-subtitle'>About Us</div>
-        <div className='card-body-description'>{props.jobDetailsFromCompany}</div>
+        <div className='card-body-description'>{TruncatedText(props?.jobDetailsFromCompany, 500 )}</div>
         {props?.minExp && <div className='card-body-minimumExperience-title'>Minimum Experience</div>}
         {props?.minExp && <div className='card-body-minimumExperience-value'>{`${props?.minExp} years`}</div>}
       </div>
